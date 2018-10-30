c00b37563bd52063342b7ff8dc9e3bac7f138d3c
regex01 <ahmetcf@gmail.com>
2018-10-29 17:40:30 -0700

    Add files via upload
    Predict the gender of given individual by their body size. M(Male) or F(Female)

---
+++ DemoGender.py
@@ -0,0 +1,22 @@
+from sklearn import tree
+
+#[height,weight,shoe size]
+
+X = [[181,80,44],[177,70,43],[160,60,38],[154,54,37],
+	 [166,65,40],[190,90,47],[175,64,39],[177,70,40],[159,55,37],
+	 [171,75,42],[181,85,43]] 
+
+Y = ['male','female','female','female','male','male','male',
+	'female','male','female','male']
+
+#clf will store our decision tree clasifier 
+
+clf  = tree.DecisionTreeClassifier()
+
+clf = clf.fit(X,Y)
+
+prediction = clf.predict([[100,90,43]])
+
+print(str(prediction))	
+
+
