001f87f21435895c3d749c9b75d0c10138d89115
Ahmet Ciftcioglu <ahmetcf@gmail.com>
2023-12-06 10:20:24 -0800

    add code

--- Codeheaven/AverageIQLevel.py
+++ Codeheaven/AverageIQLevel.py
@@ -1,8 +1,4 @@
 import pandas as pd 
-import numpy as np
-import matplotlib.pyplot as plt
-import sklearn as skt
-import seaborn as sns
 
 df= pd.read_csv(r"C:\Users\aciftcioglu\Downloads\Datasets\bankmarketing_train.csv")
 
