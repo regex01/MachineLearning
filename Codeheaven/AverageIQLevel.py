dcb2e75f03756fecdb7696bf62f4c814a52b1376
Ahmet Ciftcioglu <ahmetcf@gmail.com>
2023-12-06 12:10:49 -0800

    add codes.

--- Codeheaven/AverageIQLevel.py
+++ Codeheaven/AverageIQLevel.py
@@ -1,4 +1,3 @@
-import pandas as pd 
+import pandas as pd
 
-df= pd.read_csv(r"C:\Users\aciftcioglu\Downloads\Datasets\bankmarketing_train.csv")
-
+df = pd.read_csv(r"C:\Users\aciftcioglu\Downloads\AI\datasets\IQ_level.csv')
